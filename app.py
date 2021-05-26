fdb99b3df819c5b24b3750d5837005d58ae2cd00
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-05-26 16:36:51 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-  
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
-
-print("hola")
-if __name__ == '__main__':
-    app.run_server()
